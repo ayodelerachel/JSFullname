f61e3a758f6a6567d7a6bfa83c62dfacf32ecf4d
ayodelerachel <105463418+ayodelerachel@users.noreply.github.com>
2022-06-23 18:56:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let fullName = "Fullname: Ayodele Rachel\n";
+console.log(fullName)
+
+let height = "Height: 153m\n";
+console.log(height)
+
+let country = "Country: Nigeria\n"
+console.log(country)
+
+
+alert(fullName + height + country)
